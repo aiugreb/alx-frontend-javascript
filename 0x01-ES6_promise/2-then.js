04354958fe9c1c73a3fc64a3d2f88349888b77b2
Mohamed Amine Berguia <mohamedamineberguia@gmail.com>
2024-07-25 00:12:55 +0100

    debugg

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  const object = { status: 200, body: 'success' };
   return promise
-    .then(() => object)
-    .catch(() => new Error())
-    .finally(() => { console.log('Got a response from the API'); });
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
 }
-
